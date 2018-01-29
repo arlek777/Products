83c11e0001a73a76dc03174e55d96680ae100143
RomanKrasnobai <30289240+RomanKrasnobai@users.noreply.github.com>
2018-01-29 14:15:01 +0200

    update contact page ver-1.2

--- Products/Products/App_Start/BundleConfig.cs
+++ Products/Products/App_Start/BundleConfig.cs
@@ -29,7 +29,7 @@ namespace Products
                 "~/Scripts/app/products/products.service.js",
                 "~/Scripts/app/cart/cart.service.js",
                 "~/Scripts/app/cart/cart-controller.js",
-                "~/Scripts/app/contact/contact-controller.js));
+                "~/Scripts/app/contact/contact-controller.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap/bootstrap.js",
