15306c12a110553080e1396f9af3aa686ac3a6df
RomanKrasnobai <30289240+RomanKrasnobai@users.noreply.github.com>
2018-01-29 17:27:18 +0200

    Update contact form ver 1.3

--- Products/Products/Scripts/app/contact/contact-controller.js
+++ Products/Products/Scripts/app/contact/contact-controller.js
@@ -5,13 +5,36 @@
 
     function contactController() {
         var vm = this;
+        vm.checkValidation = true;
 
         vm.customer = {
-            fullName: null, adress: null, email: null
+            fullName: null,
+            address: null,
+            email: null
         };
 
-        vm.showAlert = function() {
+        vm.confirmContact = function() {
             alert("Form is Valid");
+        }
+
+        vm.checkContactForm = function() {
+            if (contactForm.$valid) {
+                vm.checkValidation = false;
+            } else {
+                vm.checkValidation = true;
+            }
+        }
+
+        vm.errorAlert = function(error) {
+            if (angular.isDefined(error)) {
+                if (error.required) {
+                    return "Input is required";
+                } else if (error.minlength) {
+                    return "Not valid length";
+                } else if (error.email) {
+                    return "Not valid email address";
+                }
+            }
         }
     }
 })();
