d3f40cfcb3d6e1b3bf687bb490cf46820702431f
RomanKrasnobai <30289240+RomanKrasnobai@users.noreply.github.com>
2018-01-31 18:03:41 +0200

    Add summary page

---
+++ Products/Products/Scripts/app/summary/summary-controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+    summaryController.$inject = ["cartService"];
+    angular.module("app.controllers").controller("SummaryController", summaryController);
+
+    function summaryController() {
+        var vm = this;
+        vm.productsInCart = cartService.getProducts();
+
+        vm.confirm = function () {
+            alert("TEST click");
+        }
+    }
+})();
